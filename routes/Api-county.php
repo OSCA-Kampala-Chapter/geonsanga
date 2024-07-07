423c0a9bd8f86a0e36fdd0027b0dcebea5c4fc1b
marshud <ubatamye@gmail.com>
2024-07-07 22:29:08 +0300

    Adding county operations

---
+++ routes/Api-county.php
@@ -0,0 +1,31 @@
+<?php
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
+
+
+use Steampixel\Route;
+use Uganda\Exceptions\CountyNotFoundException;
+
+$obj = new stdClass();
+
+/**
+ * County Particular operations
+ */
+// Get all County details
+Route::add('/v1/county/([a-z-0-9-]*)', function ($county) use($uganda, $obj) {
+
+  echo "Hello county";
+  // header('Content-Type: application/json');
+
+  // try {
+  //   $county_ = $uganda->county($county);
+    
+  //   $obj->count = 1;
+  //   $obj->county = $county_;
+  // } catch (CountyNotFoundException $e) {
+  //   $obj->error = "County Not Found";
+  // }
+  // echo json_encode($obj, JSON_PRETTY_PRINT);
+},'GET');
+
