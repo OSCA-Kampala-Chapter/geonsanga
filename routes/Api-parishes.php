71a8a4ffbf5be7d23fa94f31998068baabd3278d
marshud <ubatamye@gmail.com>
2024-07-08 23:48:43 +0300

    Adding villages in a single parish

--- routes/Api-parishes.php
+++ routes/Api-parishes.php
@@ -30,4 +30,31 @@ Route::add('/v1/parish/([a-z-0-9-]*)', function ($param) use($uganda, $obj) {
   echo json_encode($obj, JSON_PRETTY_PRINT);
 },'GET');
 
+// Get all parish details, without a space e.g. 
+Route::add('/v1/parish/([a-z-0-9-]*)/villages', function ($param) use($uganda, $obj) {
+
+  $parish = insertSpaceBeforeUppercase($param);
+
+  header('Content-Type: application/json');
+
+  try {
+    $villages = $uganda
+                ->parish($parish)
+                ->villages();
+
+    $count = count($villages);
+
+    $names = [];
+    foreach($villages as $village):
+    $names[] = $village->name;
+    endforeach;
+    $obj->count = $count;
+    $obj->villages = $names;
+  } catch (ParishNotFoundException $e) {
+    $obj->error = $e->getMessage();
+  }
+
+  echo json_encode($obj, JSON_PRETTY_PRINT);
+},'GET');
+
 
